675349ca5ec1adc2b1003b93f7bf657adf25612e
Mathew Lister <36172965+AngryPorkchop11@users.noreply.github.com>
2019-10-28 23:29:00 -0700

    ADDED LEXER CLASS

--- parser.rb
+++ parser.rb
@@ -33,33 +33,65 @@ def read_file(file, array) #open file if DNE then close, read line by line someh
   array #return array with [0] as one long string with no whitespace
 end
 
+class Tokens
+  def initialize
+    @tokens = Array.new
+  end
+  def loadTokens()
+    @tokens = [@point, @id, @num, @semicolon, @comma, @period, @lparen, @rparen, @equal]
+  end
+  @point = /point/#expecting L(
+  @id = /\w+/ #start
+  @num = /\d+\.\d+/ #expecting COMMA || R)
+  @semicolon = /;/ #expecting ID
+  @comma = /,/ #expecting NUM
+  @period = /\./ #end
+  @lparen = /\(/#expecting NUM
+  @rparen = /\)/#expecting PERIOD || SEMI
+  @equal = /=/ #expecting POINT
+end
 
+class Lexer
+  def initialize(input)
+    @input = input
+    @tokenized = Array.new
+    @tokens = %w(POINT ID NUM SEMICOLON COMMA PERIOD LPAREN RPAREN EQUAL)
+  end
+  def tokenize
+    #loop through possible tokens and find a match
+    # If no match exit!
+    # If match: format to 'token' and add to tokenized list
+  end
+end
 #////////////////////////////////////////////VARS///////////////////////////////////////////////////#
-input_array = ARGV #takes in command line as arguments
-#input_array = %w(test1.cpl -p) #***DEBUGGING w/out commandline input***#
-_chars = [] #array to make it easier to format input for lexing
-input_as_string = String.new #holds formatted input for lexing (one big string)
-_key = String.new
-file_name = String.new
+$input_array = ARGV #takes in command line as arguments
+#$input_array = %w(test1.cpl -p) #***DEBUGGING w/out commandline input***#
+$_chars = [] #array to make it easier to format input for lexing
+$input_as_string = String.new #holds formatted input for lexing (one big string)
+$_key = String.new
+$file_name = String.new
 #////////////////////////////VARS//////////////////////////////////////////////////////////////////#
 
 
 # validate starting input
-if validate_start(input_array) #if method returns true
-  file_name = input_array[0] #initalize file name from ARGV
-  _key = input_array[1] #key should be -s or -p
+if validate_start($input_array) #if method returns true
+  file_name = $input_array[0] #initalize file name from ARGV
+  $_key = $input_array[1] #key should be -s or -p
 end
-  puts "Processing Input File #{file_name}"
+  puts "Processing Input File #{$file_name}"
 
-if _key == "-s"
-  if read_file(input_array[0], _chars) != false #read file and store chars into an array
-    input_as_string = read_file(input_array[0], _chars).join #calling method twice feels redundant...?IDK
-    print input_as_string #***FOR TESTING***#
+if $_key == "-s"
+  if read_file($input_array[0], $_chars) != false #read file and store chars into an array
+    $input_as_string = read_file($input_array[0], $_chars).join
+    print $input_as_string #***FOR TESTING***#
   else
   puts "ERROR IN BODY"
   end
+  #tokenize = Lexer.new($input_as_string)
+  # USE: tokenize.methodName later on
 end
 
-if _key == "-p"
+if $_key == "-p"
 puts "NOT HERE YET"
 end
+
