e60a238f14c7a3af25d7746eb5daf9649e458232
Mathew Lister <36172965+AngryPorkchop11@users.noreply.github.com>
2019-11-02 10:51:29 -0700

    Redesign and finished

--- parser.rb
+++ parser.rb
@@ -21,30 +21,29 @@ def validate_start(array) #validates length of input for filename and key, in th
   false
 end
 
-def read_file(file, array) #open file if DNE then close, read line by line somehow make into one big line in array[0] for parsing and lexing
+def read_file(file, array) #open file if DNE then close, read line by line
   begin
-    #_text = File.read(file, chomp: true)
     array = File.readlines(file)
   rescue => e
     puts "File does not exist!"
     puts e
     return false
   end
-
+#strip out all whitespace (space, tabs and enters)
   array.each do |item|
     item.gsub!(/\s+/, '')
     end
-  if array.length != 3
+  if array.length == 0
     puts "No data in file!"
     return false
   end
-  array #return array with [0] as one long string with no whitespace
+  array
 end
 
 def print_tokens(ar1, ar2)
-  arr = ar1.zip(ar2)
+  arr = ar1.zip(ar2) #combine tokens and lexemes
   arr.each do |i|
-    if ar1[arr.find_index(i)] == "ID" || ar1[arr.find_index(i)] == "NUM"
+    if ar1[arr.find_index(i)] == "ID" || ar1[arr.find_index(i)] == "NUM" #if we have ID or NUM token print associating data
       puts i.join(" ")
     else
       puts ar1[arr.find_index(i)]
@@ -53,79 +52,194 @@ def print_tokens(ar1, ar2)
 end
 
 def tokenize(str, key = -1)
+  point = /"point"/#expecting L(
+  id = /[a-zA-z]/ #start expecting = || more of the id
+  num = /\d+\.?\djk
+*/ #expecting COMMA || R || more of the num)
+  semicolon = /;/ #expecting ID
+  comma = /,/ #expecting NUM
+  period = /\./ #end
+  lparen = /\(/#expecting NUM
+  rparen = /\)/#expecting PERIOD || SEMI
+  equal = /=/ #expecting POINT
+#_______________________VARS FOR MOVING AND STORAGE_______________________________________
   token_array = Array.new
   lexeme_array = Array.new
   str_array = str.chars
-  num_array = Array.new
-  lexer_array = Array.new #so my lexer can throw flags and tokens
-  point_stage = 0
+  id_flag = 0 #so we dont confuse a point as ID
   i = 0
-  char = str_array[i] #get beginning of str
+  char = str_array[i]
   next_char = str_array[i+1]
-  lexer = Lexer.new
-
-  while char != (nil)
-    point_stage = 0
-    lexer_array = lexer.match_chars(char, next_char)
-    # if lexer_array != -1 && lexer_array != 1 && lexer_array != 2
-    #   lexer_array.flatten
-    # end
-    if lexer_array == -1
-      puts "Lexical error"
-      false
-    elsif lexer_array == 1
-      #keep working on point
-      #keep going till lexer gives token or -1 --> then add
-      #move up
-      i+= 2
-      char = str_array[i]
-      next_char = str_array[i+1]
-      lexer_array = lexer.match_chars(char, next_char,1)
-      if lexer_array == 2
-        #keep working on point
-        #keep going till lexer == 0 --> then add
-        #move up
-        i+= 2
-        char = str_array[i]
-        next_char = str_array[i+1]
-        lexer_array = lexer.match_chars(char, next_char,2)
-        if lexer_array != -1 #confirms we have a complete point
-          token_array << lexer_array
-          lexeme_array << "point" << next_char
-          #move up
-          i+= 2
+#_______________________________________________________________
+  while next_char != nil
+    if id_flag == 0
+      if !!char.match(id) && !!next_char.match(equal)#best-case
+          token_array << "ID" << "EQUAL"
+          lexeme_array << char << next_char
+          i+=2
           char = str_array[i]
           next_char = str_array[i+1]
-          point_stage = 1
+          id_flag = 1 #so point dosent enter this condition
+      elsif !!next_char.match(id) #worst-case
+        temp_char = String.new
+        while !!next_char.match(id) #move up one by one
+          i+=1
+          next_char = str_array[i]
+          if !!next_char.match(id)
+            temp_char << next_char
+          end
         end
+        token_array << "ID"
+        lexeme_array << (char.concat temp_char)
+        if !!next_char.match(equal)
+          token_array << "EQUAL"
+          lexeme_array << next_char
+          i+=1
+          char = str_array[i]
+          next_char = str_array[i+1]
+          id_flag = 1 #so point dosent enter this condition
+        else
+          return false
         end
       end
-    if lexer_array == %w(SEMICOLON)
-      token_array << lexer_array
-      lexeme_array << char
-      i += 1
-      char = str_array[i]
-      next_char = str_array[i+1]
-    elsif point_stage == 0
-      token_array << lexer_array
+    end
+    if id_flag == 1 #looking for point
+      if char == 'p' #only-case
+        if next_char == 'o'
+          i += 2
+          char = str_array[i]
+          next_char = str_array[i+1]
+          if char == 'i'
+            if next_char == 'n'
+              i += 2
+              char = str_array[i]
+              next_char = str_array[i+1]
+              if char == 't'
+                if !!next_char.match(lparen)
+                  token_array << "point" << "LPAREN"
+                  lexeme_array << "point" << next_char
+                  id_flag = 0 #now that we have point we can be ready for next ID
+                  i += 2
+                  char = str_array[i]
+                  next_char = str_array[i+1]
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    if !!char.match(num) && !!next_char.match(comma) #best-case
+        token_array << "NUM" << "COMMA"
+        lexeme_array << char << next_char
+        i += 2
+        char = str_array[i]
+        next_char = str_array[i+1]
+    elsif !!next_char.match(rparen) #best-case
+      token_array << "NUM" << "RPAREN"
       lexeme_array << char << next_char
-      #move up
-      i+= 2
+      i += 2
       char = str_array[i]
       next_char = str_array[i+1]
+    elsif !!char.match(period) && !!next_char.match(num) #handling nums w/ decimal
+      temp_num = char
+      while !!next_char.match(num) #move up one by one
+        i+=1
+        next_char = str_array[i]
+        if !!next_char.match(num)
+          temp_num << next_char
+        end
+      end
+      token_array << "NUM"
+      lexeme_array << (char.concat temp_num)
+      i+=1
+      char = str_array[i]
+      next_char = str_array[i+1]
+    elsif !!next_char.match(period) #handling nums w/ decimal
+      temp_num = next_char
+      i+=2
+      next_char = str_array[i]
+      if !!next_char.match(num)
+        while !!next_char.match(num) #move up one by one
+          i+=1
+          next_char = str_array[i]
+          if !!next_char.match(num)
+            temp_num << next_char
+          end
+        end
+        token_array << "NUM"
+        lexeme_array << (char.concat temp_num)
+        i+=1
+        char = str_array[i]
+        next_char = str_array[i+1]
+      else
+        return false
+      end
+    elsif !!next_char.match(num) #worst-case
+      temp_num = String.new
+      while !!next_char.match(num) #move up one by one
+        i+=1
+        next_char = str_array[i]
+        if !!next_char.match(num)
+          temp_num << next_char
+        end
+        if !!next_char.match(period) #handle the decimal
+          i+=1
+          next_char = str_array[i]
+          temp_num << "." << next_char
+          if next_char.match(num) == false
+            return false #NUM must come after the decimal
+          end
+        end
+      end
+      token_array << "NUM"
+      lexeme_array << (char.concat temp_num)
+      if !!next_char.match(comma)
+        token_array << "COMMA"
+        lexeme_array << next_char
+        i+=1
+        char = str_array[i]
+        next_char = str_array[i+1]
+      elsif !!next_char.match(rparen)
+        token_array << "RPAREN"
+        lexeme_array << next_char
+        i+=1
+        char = str_array[i]
+        next_char = str_array[i+1]
+      end
+      false
+    end
+    if !!char.match(semicolon) #only-case
+      if !!next_char.match(id) #just checking
+        token_array << "SEMICOLON"
+        lexeme_array << char
+        i += 1
+        char = str_array[i]
+        next_char = str_array[i+1]
+      else
+        return false #ID has to come after a semicolon but I dont want to tokenize ID here
+      end
+    end
+    if !!char.match(period)
+      if next_char == nil #only-case
+        token_array << "PERIOD"
+        lexeme_array << char
+      end
     end
   end
-  num_array = get_nums(token_array.flatten, lexeme_array)
-  puts "Lexical and Syntax analysis passed"
-  if key == -1
+  if token_array.length == 0 #couldnt find any tokens but it didnt fail any tests -- thus we have an uncaught error with data on file
+    return false
+  end
+  num_array = get_nums(token_array.flatten, lexeme_array) #get the 6 nums for printing
+  puts "Lexical and Syntax analysis passed" #if syntax is wrong it shouldnt pass the tests above same with lexer
+  if key == -1 #if you want to see the tokens processed
     print_tokens(token_array.flatten, lexeme_array)
-  elsif key == "-s"
+  elsif key == "-s" #scheme output
     print_s(num_array)
-  elsif key == "-p"
+  elsif key == "-p" #prolog output
     print_p(num_array)
   end
-
-end
+  end
 
 def get_nums(ar1, ar2)
   arr = ar1.zip(ar2)
@@ -143,59 +257,22 @@ def print_s(ar1)
 end
 
 def print_p(ar1)
-
+puts "query(line(point2d(#{ar1[0]}, #{ar1[1]}), point2d(#{ar1[2]}, #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(triangle(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(vertical(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(horizontal(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(equilateral(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(isosceles(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(right(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(scalene(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(acute(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "query(obtuse(point2d(#{ar1[0]} #{ar1[1]}), point2d(#{ar1[2]} #{ar1[3]}), point2d(#{ar1[4]} #{ar1[5]})))"
+puts "writeln(T) :- write(T), nl."
+puts "main:- forall(query(Q), Q-> (writeln:'yes')) ; writeln('no'))),"
+puts "       halt."
 end
 
-class Lexer
-  def initialize
-    @point = /"point"/#expecting L(
-    @id = /[a-zA-z]/ #start expecting = || more of the id
-    @num = /\d+\.?\d*/ #expecting COMMA || R || more of the num)
-    @semicolon = /;/ #expecting ID
-    @comma = /,/ #expecting NUM
-    @period = /\./ #end
-    @lparen = /\(/#expecting NUM
-    @rparen = /\)/#expecting PERIOD || SEMI
-    @equal = /=/ #expecting POINT
-  end
-  def match_chars(c1, c2, stage = 0) # takes in two chars (one is the runner/lookahead and a flag for point stages)
-    if stage == 0
-      if !!c1.match(@id) && !!c2.match(@equal)
-        return %w(ID EQUAL)
-      end
-      if c1 == 'p'
-        if c2== 'o'
-          return 1
-        end
-      end
-      if !!c1.match(@num) && !!c2.match(@comma)
-        return %w(NUM COMMA)
-      end
-      if !!c1.match(@num) && !!c2.match(@rparen)
-        return %w(NUM RPAREN)
-      end
-      if !!c1.match(@semicolon) && !!c2.match(@id)
-        return %w(SEMICOLON)
-      end
-      if !!c1.match(@period) && c2 == nil
-        return %w(PERIOD)
-      end
-      -1
-      elsif stage == 1
-        if c1 == 'i' && c2 == 'n'
-          return 2
-        else
-          return -1
-        end
-      elsif stage == 2
-        if c1 == 't' && !!c2.match(@lparen)
-          return %w(POINT LPAREN)
-        else
-          return -1
-        end
-      end
-  end
-end
+
 #////////////////////////////////////////////VARS///////////////////////////////////////////////////#
 input_array = ARGV #takes in command line as arguments
 #input_array = %w(test1.cpl -s) #***DEBUGGING w/out commandline input***#
@@ -208,19 +285,11 @@ file_name = String.new
 if validate_start(input_array) #if method returns true
   file_name = input_array[0] #initalize file name from ARGV
   _key = input_array[1] #key should be -s or -p
-end
   puts "Processing Input File #{file_name}"
-
-#if -s
-if _key == "-s"
-  if read_file(input_array[0], _chars) != false #read file and store chars into an array
-    input_as_string = read_file(input_array[0], _chars).join
-    if tokenize(input_as_string, _key) == false
-      puts "ERROR IN LEXER"
-    end
+end
+if read_file(input_array[0], _chars) != false #read file and store chars into an array
+  input_as_string = read_file(input_array[0], _chars).join
+  if tokenize(input_as_string, _key) == false #lexical and syntax analysis
+    puts "ERROR IN LEXER"
   end
 end
-
-#if -p
-# if _key == "-p"
-# puts "NOT HERE YET"
